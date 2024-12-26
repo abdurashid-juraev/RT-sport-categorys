5ff2ce4f6296678a1b24a16eaab0a82e3f868bfb
Abdurashid <aj6243434@gmail.com>
2024-12-26 14:54:22 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".card");
+// document.addEventListener("DOMContentLoaded", () => {
+const cards = document.querySelectorAll(".card");
 
-  cards.forEach((card) => {
-    const cardLinks = card.querySelectorAll(".card__link");
-    const tabContents = card.querySelectorAll(".item");
+cards.forEach((card) => {
+  const cardLinks = card.querySelectorAll(".card__link");
+  const tabContents = card.querySelectorAll(".item");
 
-    cardLinks.forEach((link) => {
-      link.addEventListener("click", (event) => {
-        event.preventDefault();
-        event.stopPropagation();
+  cardLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      event.stopPropagation();
 
-        cardLinks.forEach((cardLink) => cardLink.classList.remove("active"));
-        link.classList.add("active");
+      cardLinks.forEach((cardLink) => cardLink.classList.remove("active"));
+      link.classList.add("active");
 
-        const targetTab = link.dataset.tab;
-        tabContents.forEach((content) => {
-          content.classList.remove("active");
-          if (content.dataset.tabContent === targetTab) {
-            content.classList.add("active");
-          }
-        });
+      const targetTab = link.dataset.tab;
+      tabContents.forEach((content) => {
+        content.classList.remove("active");
+        if (content.dataset.tabContent === targetTab) {
+          content.classList.add("active");
+        }
       });
     });
   });
 });
+// });
